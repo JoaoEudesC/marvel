dc1f7af46e208c2cd8afe84636aff4bcde7cce9b
joaoeudes2501 <joaoeudes2501200@gmail.com>
2022-09-30 22:02:09 +0100

    melhoria 5

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 console.log("estou funcionando");
 
 
-
 function desenvolver(){
-    alert(" desenvolvido por joao carvalho")
+alert(" desenvolvido por joao carvalho")
 };
+
+
+
 desenvolver()
+
+
 
 var b= "bruce banner"
 var c= "hydra"
 var a = "ben"
-
-
-
-
 
 
 
@@ -22,16 +22,30 @@ var hulk = prompt(" qual o nome humano do hulk?");
 alert("voce esta correto se digitou ," + b) ;       
  
 
-
-
 var aranha = prompt("qual o nome do tio do homem aranha ?")
 
-
 alert("voce esta correto se digitou ," + a)    
+
 
 var america = prompt("qual a organizaçao inimiga do capitao america?");
   
 alert("voce esta correto se digitou ," + c)    
+
+
+
+var h1=document.querySelector("h1")
+
+h1.addEventListener("mouseenter" , cor)
+
+function cor ()
+{
+if(h1.style.backgroundColor === "darkred")
+{h1.style.backgroundColor = "blue"
+}
+ else{h1.style.backgroundColor = "darkred"}
+
+}
+
 
 
 
